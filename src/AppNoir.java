fff78f8a83877d443f91c32c49f9a0cf1610a79d
Alexander Bazo <alexanderbazo@googlemail.com>
2021-11-16 10:02:20 +0100

    Add solution code

--- src/AppNoir.java
+++ src/AppNoir.java
@@ -1,25 +1,68 @@
 import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.colors.Color;
+import de.ur.mi.oop.graphics.Image;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
 
 public class AppNoir extends GraphicsApp {
 
-    private static final Color RED = new Color(234, 49, 63); // "Selbstgemischter" RGB-Farbe (rot)
-    private static final Color YELLOW = new Color(234, 182, 56); // "Selbstgemischter" RGB-Farbe (gelb)
-    private static final Color GREEN = new Color(76, 149, 80); // "Selbstgemischter" RGB-Farbe (grün)
-    private static final Color BLUE = new Color(53, 129, 184); // "Selbstgemischter" RGB-Farbe (blau)
-    private static final Color TURQUOISE = new Color(9, 82, 86); // "Selbstgemischter" RGB-Farbe (türkis)
-    private static final Color PURPLE = new Color(55, 39, 114); // "Selbstgemischter" RGB-Farbe (lila)
-    private static final Color CREAM = new Color(241, 255, 250); // "Selbstgemischter" RGB-Farbe (creme)
-    private static final Color GREY = new Color(47, 61, 76); // "Selbstgemischter" RGB-Farbe (grau)
+
+    private static final int WINDOW_WIDTH = 900;
+    private static final int WINDOW_HEIGHT = 600;
+    private static final String IMAGE_PATH = "assets/atlantic-puffin.png";
+    private static final int FRAME_DELAY_BETWEEN_MODE_TOGGLE = 300;
+
+    private Image originalImage;
+    private Image grayscaleImage;
+    private boolean drawGrayscaleImage;
+    private int frameCounter = 0;
+
     @Override
     public void initialize() {
-        setCanvasSize(500, 500);
+        setCanvasSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        drawGrayscaleImage = false;
+        originalImage = createImage(IMAGE_PATH, false);
+        grayscaleImage = createImage(IMAGE_PATH, true);
+    }
+
+    private Image createImage(String path, boolean grayscale) {
+        Image image = new Image(0, 0, path);
+        image.setWidth(getWidth(), true);
+        image.setHeight(getHeight(), true);
+        if (grayscale) {
+            int[][] greyscalePixels = getGreyscalePixelsFor(image);
+            image.setPixelArray(greyscalePixels);
+        }
+        return image;
+    }
+
+    private int[][] getGreyscalePixelsFor(Image image) {
+        int[][] pixels = image.getPixelArray();
+        for (int x = 0; x < pixels.length; x++) {
+            for (int y = 0; y < pixels[0].length; y++) {
+                Color color = new Color(pixels[x][y]);
+                int colorAverage = (color.red() + color.green() + color.blue()) / 3;
+                Color grayscaleColor = new Color(colorAverage, colorAverage, colorAverage);
+                pixels[x][y] = grayscaleColor.toInt();
+            }
+        }
+        return pixels;
     }
 
     @Override
     public void draw() {
-        drawBackground(CREAM);
+        if(frameCounter % FRAME_DELAY_BETWEEN_MODE_TOGGLE == 0) {
+            drawGrayscaleImage = !drawGrayscaleImage;
+        }
+        frameCounter++;
+        drawImage();
+    }
+
+    private void drawImage() {
+        if (drawGrayscaleImage) {
+            grayscaleImage.draw();
+        } else {
+            originalImage.draw();
+        }
     }
 
     public static void main(String[] args) {
